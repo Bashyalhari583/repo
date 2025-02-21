8f4a07d93202fdfbc647b7b87e6366216371167d
biju chaudhary <bjtharu7@gmial.com>
2025-02-21 15:13:39 +0545

    sample of a component

---
+++ src/components/Sample.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sample = () => {
+    return (
+        <div>
+                this si the nr ajsdn aj,sdf a;ks dfma ejfa sdmfk ajerrfnjsd s
+        </div>
+    );
+}
+
+export default Sample;
